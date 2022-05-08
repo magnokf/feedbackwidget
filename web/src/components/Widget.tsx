217c344826bde99aaa2f6ce6140f84ebd97161dd
magnokf <mkfelipe.rj@gmail.com>
2022-05-08 09:45:48 -0300

    first commit

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,8 @@
+import { ChatTeardropDots } from 'phosphor-react';
+
+export function Widget() {
+
+  return <button className='bg-violet-500 rounded'>
+    <ChatTeardropDots className='w-6 h-6' />
+  </button>
+}
